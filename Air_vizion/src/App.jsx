f1a58f0470a6b670b8e32d1ea5d78b6395d78232
shamina-dev <shamina-dev.shaminapalansooriya@gmail.com>
2024-12-11 08:18:36 +0530

    altered components

--- Air_vizion/src/App.jsx
+++ Air_vizion/src/App.jsx
@@ -1,28 +1,30 @@
-import Header from './components/Header.jsx'
-import System from './components/System.jsx';
-import Home from './components/Home.jsx';
-import Sensors from './components/Sensors.jsx';
-import Implementation from './components/Implementation.jsx';
-import Feedback from './components/Feedback.jsx';
-import Contact from './components/Contact.jsx';
-import { Route , Routes } from "react-router-dom"
+import Header from "./components/Header.jsx";
+import System from "./components/System.jsx";
+import Home from "./components/Home.jsx";
+import Sensors from "./components/Sensors.jsx";
+import Implementation from "./components/Implementation.jsx";
+import Feedback from "./components/Feedback.jsx";
+import Contact from "./components/Contact.jsx";
+import Solution from "./components/Solution.jsx";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return(
+  return (
     <>
-      <Header/>
-      <div className='container'>
+      <Header />
+      <div className="container">
         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/System' element={<System/>}/>
-          <Route path='/Sensors' element={<Sensors/>}/>
-          <Route path='/Implementation' element={<Implementation/>}/>
-          <Route path='/Feedback' element={<Feedback/>}/>
-          <Route path='/Contact' element={<Contact/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/System" element={<System />} />
+          <Route path="/Sensors" element={<Sensors />} />
+          <Route path="/Implementation" element={<Implementation />} />
+          <Route path="/Solution" element={<Solution />} />
+          <Route path="/Feedback" element={<Feedback />} />
+          <Route path="/Contact" element={<Contact />} />
         </Routes>
       </div>
     </>
   );
 }
 
-export default App
+export default App;
--- Air_vizion/src/components/Header.jsx
+++ Air_vizion/src/components/Header.jsx
@@ -1,28 +1,43 @@
 import { Link } from "react-router-dom";
 
-function Header(){
-  return(
+function Header() {
+  return (
     <header>
-      
-        <nav className="nav">
-        <Link to="/" className="site-titel">Kandy Air</Link>
-          <ul>
-            <li> <Link to="/system">System</Link></li>
-            <li><Link to="/sensors">Sensors</Link> </li>
-            <li><Link to="/implementation">Implementation</Link> </li>
-            <li><Link to="/feedback">Feedback</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-          </ul>
-        </nav>
+      <nav className="nav">
+        <Link to="/" className="site-titel">
+          Air Vizion
+        </Link>
+        <ul>
+          <li>
+            {" "}
+            <Link to="/system">System</Link>
+          </li>
+          <li>
+            <Link to="/Solution">Solution</Link>
+          </li>
+          <li>
+            <Link to="/sensors">Sensors</Link>{" "}
+          </li>
+          <li>
+            <Link to="/implementation">Implementation</Link>{" "}
+          </li>
+          <li>
+            <Link to="/feedback">Feedback</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
 
-function CustomLink({to,children,...props}){
-  const path = window.location.pathname
+function CustomLink({ to, children, ...props }) {
+  const path = window.location.pathname;
 
-  return(
-    <li className={path === to ? "active":""}>
+  return (
+    <li className={path === to ? "active" : ""}>
       <Link to={to} {...props}>
         {children}
       </Link>
@@ -30,4 +45,4 @@ function CustomLink({to,children,...props}){
   );
 }
 
-export default Header
+export default Header;
