59cbf4a8a91bbee5803854bad6954da3cae068a3
Milinda Bandara <milindabandara27@gmail.com>
2025-02-01 10:56:47 +0530

    project

--- Air_Vizion/resources/js/Layouts/RegisterLayout.jsx
+++ Air_Vizion/resources/js/Layouts/RegisterLayout.jsx
@@ -5,7 +5,7 @@ export default function GuestLayout({ children }) {
     return (
         <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
             <div>
-                <Link href="/">Register</Link>
+                <Link href="/"></Link>
             </div>
 
             <div className="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
